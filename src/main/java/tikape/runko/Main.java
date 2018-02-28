c89d4fe884b31769574f00c10f4d17f50b6b5e27
Viannaiv <viva95@windowslive.com>
2018-02-28 15:40:48 +0200

    heroku muutoksia

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -16,6 +16,11 @@ import tikape.runko.domain.SmoothieRaakaAine;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
+        
         Database database = new Database("jdbc:sqlite:smoothie.db");
         database.init();
 
